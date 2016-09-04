62be018f989930da803e7f168117c962b6e760fc
Aaron H <hongnghigia@gmail.com>
2016-09-04 22:20:27 +1000

    Minor changes

--- tree.py
+++ tree.py
@@ -4,22 +4,33 @@ class Tree:
 	def __init__(self):
 		self.nodes = []
 		self.length = 0
-		self.identifier = 0
+		self.count = 0
 
 	def nodes(self):
 		return self.nodes
 
-	def getLength(self):
-		return self.length
+	def __repr__(self):
+		return str(self.nodes)
 
 	def __len__(self):
 		return self.length
 
 	def addNode(self, node):
-		self.identifier += 1
 		self.nodes.append(node)
 
-	def createNode(self,state,identifier,parent=None):
-		node = TreeNode(state, identifier+str(self.identifier), parent)
-
+	def createNode(self, state, op, path, parent=None):
+		node = TreeNode(state, op, "N"+str(self.count), path, parent)
+		self.count += 1
 		return node
+
+	def decreaseCount(self):
+		self.count -= 1
+
+	def incLength(self):
+		self.length += 1
+
+	def hasNode(self, target):
+		if target in self.nodes:
+			return True
+		else:
+			return False
--- treenode.py
+++ treenode.py
@@ -1,9 +1,11 @@
 class TreeNode:
-	def __init__(self, state, identifier, parent = None):
+	def __init__(self, state, op, identifier, path, parent = None):
 		self.children = []
+		self.op = op
 		self.state = state
 		self._identifier = identifier
 		self.parent = parent
+		self.path = path + [self._identifier]
 
 	def isRoot(self):
 		return not self.parent
@@ -15,12 +17,32 @@ class TreeNode:
 			return True
 
 
+	def __repr__(self):
+		return self.state
+
 	@property
 	def identifier(self):
 		return self._identifier
+
+	def getParent(self):
+		return self.parent
+		
 	
 	def addChild(self, identifier):
 		self.children.append(identifier)
 
 	def getState(self):
 		return self.state
+
+	def getChildren(self):
+		return self.children
+
+	def getOp(self):
+		return self.op
+
+	def getPath(self):
+		return self.path
+
+	def addPath(self, node):
+		self.path.append(node)
+
