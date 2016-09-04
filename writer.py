3a74d54e2458c2f6e65e517cfead0886293d3bdd
Aaron H <hongnghigia@gmail.com>
2016-09-04 22:19:59 +1000

    Added diagnostic printing mode

--- writer.py
+++ writer.py
@@ -11,3 +11,29 @@ class Writer:
         self.fout = open(self.fname + ".txt", "a")
         self.fout.write(str(op) + "\t" + str(state) + "\t" + str(cost) + "\n")
         self.fout.close()
+
+
+    def diagWrite(self,op ,state, cost,reason):
+		
+    	_op = self.convert(op)
+    	self.fout = open(self.fname + ".txt", "a")
+    	self.fout.write(str(_op) + "\t" + str(state) + "\t" + str(cost) + "\t" + reason + "\n")
+    	self.fout.close()
+
+    def convert(self, op):
+		if op == 0:
+			tmp = "START"
+		elif op == 1:
+			tmp = "1R"
+		elif op == 2:
+			tmp = "2R"
+		elif op == 3:
+			tmp = "3R"
+		elif op == -1:
+			tmp = "1L"
+		elif op == -2:
+			tmp = "2L"
+		elif op == -3:
+			tmp = "3L"
+
+		return tmp
